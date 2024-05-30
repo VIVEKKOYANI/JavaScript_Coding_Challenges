0d364f47a252262a5b550ab1208b7961adaa4e9d
vivek koyani <vivek.koyani@gmail.com>
2024-05-30 23:04:15 +0530

    format code

--- arrayequal.js
+++ arrayequal.js
@@ -1,11 +1,11 @@
 const arrysAreEqual = (arr1, arr2) => {
-    if(arr1.length !== arr2.length){
-        return false;
-    }
+  if (arr1.length !== arr2.length) {
+    return false;
+  }
 
-    return arr1.every((cal, index) => cal === arr2[index]);
-}
+  return arr1.every((cal, index) => cal === arr2[index]);
+};
 
-console.log(arrysAreEqual([1,2,3], [1,2,3]));
-console.log(arrysAreEqual([1,2,3], [1,2,4]));
+console.log(arrysAreEqual([1, 2, 3], [1, 2, 3]));
+console.log(arrysAreEqual([1, 2, 3], [1, 2, 4]));
 console.log(arrysAreEqual([], []));
