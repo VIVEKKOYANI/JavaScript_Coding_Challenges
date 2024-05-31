e2dabedc42a0be86ca3a700e102c44c7014404a9
vivek koyani <vivek.koyani@gmail.com>
2024-05-31 23:13:25 +0530

    changes add for ascii char

---
+++ asciiChar.js
@@ -0,0 +1,18 @@
+const isUpperCase = (char) => {
+    if(char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 95){
+        return true
+    }
+    return false;
+
+    // char === char.isUpperCase();
+}
+
+const isLowerCase = (char) => {
+    if(char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122){
+        return true
+    }
+    return false;
+}
+
+console.log(isUpperCase('S'));
+console.log(isLowerCase('S'));
