900eaadce996745b8d8da815cbb128fe42dcfaae
vivek koyani <vivek.koyani@gmail.com>
2024-06-26 10:06:42 +0530

    format code

--- convert12to24.js
+++ convert12to24.js
@@ -1,12 +1,12 @@
 const convert12to24 = (time12h) => {
-    const [time, modifier] = time12h.split(" ");
-    let [hours, minutes] = time.split(":");
-    if(hours === "12") hours = "00";
+  const [time, modifier] = time12h.split(" ");
+  let [hours, minutes] = time.split(":");
+  if (hours === "12") hours = "00";
 
-    if(modifier === 'PM') hours = parseInt(hours) + 12;
+  if (modifier === "PM") hours = parseInt(hours) + 12;
 
-    return `${hours}:${minutes}`
-}
+  return `${hours}:${minutes}`;
+};
 
 console.log("", convert12to24("02:12 PM"));
 console.log("", convert12to24("12:00 PM"));
