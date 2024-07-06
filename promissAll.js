d58726dd2de1eb7e8340bc0bc8fcb1b320d2cf12
vivek koyani <vivek.koyani@gmail.com>
2024-07-06 10:53:47 +0530

    chnages for promise all

--- promissAll.js
+++ promissAll.js
@@ -6,6 +6,33 @@ function showText(text, time) {
   });
 }
 
-Promise.all([showText("heeloo", 100), showText("hi", 100)]).then((value) =>
+function showText1(text, time) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        reject(text);
+      }, time);
+    });
+  }
+
+// Promise all own;
+
+function myPromiseAll(promises) {
+  let result = [];
+
+  return new Promise((resolve, reject) => {
+      promises.forEach((p, index) => {
+          p.then((res) => {
+              result.push(res)
+              console.log("result", result, index , promises.length - 1);
+              if(index === promises.length - 1){
+              console.log("result", result);
+            resolve(result);
+        }
+      }).catch((err) => reject(err));
+    });
+  });
+}
+
+myPromiseAll([showText("heeloo", 100), showText1("hi", 100)]).then((value) =>
   console.log("value", value)
 );
